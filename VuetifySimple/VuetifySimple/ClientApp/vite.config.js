92f12fef3887a57b8b81a01b74be51a15f2a4588
horaceyeh <horaceyeh.web@gmail.com>
2023-02-18 10:44:27 +0800

    1. remove ClientApp 2. yarn create vuetify set project name ClientApp 3. set https and proxy in vite config

---
+++ VuetifySimple/VuetifySimple/ClientApp/vite.config.js
@@ -0,0 +1,53 @@
+// Plugins
+import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+//develop https
+import mkcert from 'vite-plugin-mkcert'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+    mkcert(),
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 44403,
+    https: true,
+    strictPort : true,
+    proxy: {
+      '/api' : {
+        target: 'https://localhost:7111',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  },
+})
